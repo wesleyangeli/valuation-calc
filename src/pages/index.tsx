3495ba118cf0078cbb84c6aff225a57393d81373
wesley angeli <wesleyangeli@gmail.com>
2024-02-16 16:32:56 -0300

    style: espacamento tab

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FormBazin } from "@/components/FormBazin";
-import { Tabs, TabsProps } from "antd";
+import { Col, Row, Tabs, TabsProps } from "antd";
 import { v4 as uuid } from "uuid";
 import { FormGraham } from "@/components/FormGraham";
 
@@ -19,8 +19,10 @@ const items: TabsProps["items"] = [
 
 export default function Home() {
   return (
-    <>
-      <Tabs items={items} style={{ minHeight: "100vh", padding: 8 }} />
-    </>
+    <Row gutter={[24, 24]} style={{ padding: 12 }} justify={"center"}>
+      <Col xs={24} sm={24} md={22} lg={16} xl={16}>
+        <Tabs items={items} />
+      </Col>
+    </Row>
   );
 }
